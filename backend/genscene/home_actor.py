fe664fbea136cff79081b8d46b885f66bbbc6a44
jmcarnahan <jmcarny@gmail.com>
2024-05-26 18:31:40 -0700

    updated to all assistant files

--- backend/genscene/home_actor.py
+++ backend/genscene/home_actor.py
@@ -17,23 +17,20 @@ class HomeActor(Actor):
     def get_instructions(self) -> str:
         return """"You are helpful assistant on a wide range of topics. You will respond in a professional manner
 to all questions and you will ask for clarity when the question is not clear. You will also provide a summary of the 
-conversation at the end of the interaction."""
+conversation at the end of the interaction"""
     
     # overriden
     def get_description(self) -> str:
         return "An example tool to interact with a plain chatbot"
-
+    
     # overriden
-    def get_assistant_files(self) -> Dict[str, io.BytesIO]:
+    def get_code_resource_files(self) -> Dict[str, io.BytesIO]:
         return {}
     
     # overriden
-    def get_tools_list(self) -> List[Any]:
+    def get_tools(self) -> List[Any]:
         return [
             {"type": "code_interpreter"},
         ]
-    
-    def call_function(self, function_name: str, arguments: Dict[str, Any]) -> str:
-        raise NotImplementedError
 
 
