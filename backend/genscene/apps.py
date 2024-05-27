bccfb1b30a18572e1216d799176c3dc27813cb71
jmcarnahan <jmcarny@gmail.com>
2024-05-26 21:20:53 -0700

    now dynamically load the actors

--- backend/genscene/apps.py
+++ backend/genscene/apps.py
@@ -1,11 +1,14 @@
 from django.apps import AppConfig
 from django.conf import settings
-from .home_actor import HomeActor
-from .database_actor import DatabaseActor
+from .actors.home_actor import HomeActor
+from .actors.database_actor import DatabaseActor
 # from .database_actor import DatabaseActor
 from .actor import Actor
 from typing import List
 import logging
+import os
+import glob
+import importlib
 
 LOGGER = logging.getLogger(__name__)
 
@@ -21,13 +24,21 @@ class GensceneConfig(AppConfig):
         self.deployment = openai_config.deployment()
         LOGGER.info(f"Created the openai client: {self.client} and deployment: {self.deployment}")
 
-        # should do this dynamically
+        actor_classes = []
+        actor_files = glob.glob(os.path.join(os.path.dirname(__file__), "actors", "*_actor.py"))
+        for actor_file in actor_files:
+            print(f"actor_file: {actor_file}")
+            class_file = os.path.splitext(os.path.basename(actor_file))[0]
+            class_name = class_file.replace("_", " ").title().replace(" ", "")
+            module = importlib.import_module(f".actors.{class_file}", package='genscene')
+            cls = getattr(module, class_name)
+            actor_classes.append(cls)
+
         self.actors = {}
-        # actor_classes = [HomeActor]
-        actor_classes = [HomeActor, DatabaseActor]
         for actor_class in actor_classes:
             actor = actor_class(self.client, self.deployment)
             self.actors[actor.get_name()] = actor
+            LOGGER.info(f"Created actor: {actor.get_name()}")
 
         return super().ready()
 
